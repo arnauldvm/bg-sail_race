8bcb6d3b11408cd457aa1fd333ff6c5ea7f371f1
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2020-04-20 22:06:40 +0200

    fix(package): do not export members used only internally

--- sail_race/__init__.py
+++ sail_race/__init__.py
@@ -1,6 +1,3 @@
-from .draws import die, draw, draw36
-from .wind import init_bearing, Wind, WindIterator
-from .wind import INIT_SPEED, MIN_SPEED, GALE_SPEED
 from .boat_bearing import BoatBearing
 from .point_of_sail import PointOfSail
 
