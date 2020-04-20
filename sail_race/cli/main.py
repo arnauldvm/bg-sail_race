b7bb6c98f52707063586a26325bddcdc45903ff6
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2020-04-20 23:50:32 +0200

    fix(cli): typo when analysing args

--- sail_race/cli/main.py
+++ sail_race/cli/main.py
@@ -20,7 +20,7 @@ def main():
     args = parse_args()
     if args.subcommand == 'play':
         play.main(args)
-    if args.subcommand == 'tables':
+    elif args.subcommand == 'tables':
         tables.main(args)
     else:
         raise(f"Unexpected subcommand { args.subcommand }")
