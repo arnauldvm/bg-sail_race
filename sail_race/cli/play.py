abe8c0bd8e6964cc400bb95b8f79d6e78c82d7ca
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2020-05-03 22:54:29 +0200

    fix(cli/play): add missing description for -i option

--- sail_race/cli/play.py
+++ sail_race/cli/play.py
@@ -9,7 +9,7 @@ def fill_argparser(parser):
     parser.description = DESCRIPTION
     parser.add_argument('-s', '--seed', type=int, help="ignore for random seed (systime based)")
     parser.add_argument('-n', '--count', type=int, required=True)
-    parser.add_argument('-i', '--initial-bearing', type=int)
+    parser.add_argument('-i', '--initial-bearing', type=int, help="ignore for random initial bearing")
 
 
 def play(seed_value, count, initial_bearing):
